e913644f991fe14b851f38dcd3e1164482c8ff35
juliacygong <jgong@g.hmc.edu>
2025-09-14 13:51:21 -0700

    Update scan.sv

--- lab03/fpga/src/scan.sv
+++ lab03/fpga/src/scan.sv
@@ -5,12 +5,12 @@
 // This module scans the keyboards for inputs
 
 module scan(input logic clk, reset,
-            input logic [3:0] cols,
+            input logic [3:0] cols_sync,
             output logic [3:0] key_col, 
             output logic [7:0] key_val
 );
 
-logic [3:0] key_row, key_col, rows;
+logic [3:0] key_row, rows;
 logic [1:0] row_num;
 logic key_in;
 
@@ -38,10 +38,10 @@ always_comb begin
     key_row = 4'b0000;
     key_col = 4'b0000;
     key_val = 8'b0000_0000;
-    if (cols != 4'b1111) begin // receives an input so one goes low
+    if (cols_sync != 4'b1111) begin // receives an input so one goes low
         key_row = ~rows; // inverts bits so that row ON is in one hot encoding
-        key_col = ~cols; // inverts bits so that col ON is in one hot encoding
-        key_val = {key_row, key_col}
+        key_col = ~cols_sync; // inverts bits so that col ON is in one hot encoding
+        key_val = {key_row, key_col};
     end
 
 end
