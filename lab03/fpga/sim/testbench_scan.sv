5319a16c50b690b33580eb46e2b8c6573a3206f5
juliacygong <jgong@g.hmc.edu>
2025-09-16 01:02:40 -0700

    Update testbench_scan.sv

--- lab03/fpga/sim/testbench_scan.sv
+++ lab03/fpga/sim/testbench_scan.sv
@@ -8,12 +8,12 @@
 
 module testbench_scan();
 logic clk, reset;
-logic [3:0] key_col, cols_sync;
+logic [3:0] key_col, cols, key_row;
 logic [7:0] key_val;
 logic [31:0] errors;
 
 // instantiate module
-scan dut(clk, reset, cols_sync, key_col, key_val);
+scan dut(clk, reset, cols, key_row, key_col, key_val, led_val);
 
 // generate clock
 always
@@ -23,38 +23,46 @@ always
 
 initial begin
     errors = 0;
-    reset = 1; #1; reset = 0; #1
+    reset = 0; #1; reset = 1; #1;
 
     // assertions for each row and column
-    wait (dut.rows == 4'b1110); cols_sync = 4'b1110; #1;
+    cols = 4'b1110; #5;
+	wait (key_row == 4'b1110); #1
     assert (key_val === 8'b0001_0001) else begin
-        $display ("Error: row = %b and col = %b", dut.rows, cols_sync);
+        $display ("Error: row = %b and col = %b", key_row, cols);
         errors = errors + 1;
     end
 
     #10
 
-    wait (dut.rows == 4'b1101); cols_sync = 4'b1101; #1;
+    cols = 4'b1101; #1;
+	wait (key_row == 4'b1101); #1
     assert (key_val === 8'b0010_0010) else begin
-        $display ("Error: row = %b and col = %b", dut.rows, cols_sync);
+        $display ("Error: row = %b and col = %b", key_row, cols);
         errors = errors + 1;
     end
 
     #10
 
-    wait (dut.rows == 4'b1011); cols_sync = 4'b1011; #1;
+	cols = 4'b1011; #1;
+    wait (key_row == 4'b1011); #1
     assert (key_val === 8'b0100_0100) else begin
-        $display ("Error: row = %b and col = %b", dut.rows, cols_sync);
+        $display ("Error: row = %b and col = %b", key_row, cols);
             errors = errors + 1;
     end
 
     #10
 
-    wait (dut.rows == 4'b0111); cols_sync = 4'b0111; #1;
+    cols = 4'b0111; #1;
+	wait (key_row == 4'b0111); #1
     assert (key_val === 8'b1000_1000) else begin
-        $display ("Error: row = %b and col = %b", dut.rows, cols_sync);
+        $display ("Error: row = %b and col = %b", key_row, cols);
             errors = errors + 1;
     end
+	
+	$display("All tests completed with %d errors", errors);
+    $stop;
+	
 end
 
 endmodule
