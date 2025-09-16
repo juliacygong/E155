bf7f1b6aebb77efaaf0a2306f4ef8d543bb953b3
juliacygong <jgong@g.hmc.edu>
2025-09-16 14:16:01 -0700

    Updating shifts for scan.sv

--- lab03/fpga/src/scan.sv
+++ lab03/fpga/src/scan.sv
@@ -13,10 +13,9 @@ module scan(input logic clk, reset,
 			output logic [2:0] led_col
 );
 
-logic [3:0] rows, c_sync, cols_sync;
-logic [1:0] row_num;
+logic [3:0] row_num, c_sync, cols_sync;
+logic row_stop;
 logic key_in;
-
 
 // synchronizer for keypad inputs
 always_ff @(posedge clk, negedge reset) begin
@@ -31,35 +30,26 @@ always_ff @(posedge clk, negedge reset) begin
 end
 
 always_ff @(posedge clk, negedge reset) begin
-    if (~reset)
-        row_num <= 0;
+	if (~reset)
+        key_row <= 4'b1110; 
+    else if (row_stop) begin // stop scanning rows when input reciepted
+        key_row <= key_row:
+    end
     else
-        row_num <= row_num + 1'b1;
+        key_row <= {key_row[2:0], key_row[3]};
 end
 
-always_comb begin
-    // set the bit low for specific bit number
-    case(row_num)
-        2'b00:      rows = 4'b1110;
-        2'b01:      rows = 4'b1101;
-        2'b10:      rows = 4'b1011;
-        2'b11:      rows = 4'b0111;
-        default:    rows = 4'b1111;
-    endcase
-end
-
-assign key_row = rows; 
 
 always_comb begin
     key_col = 4'b0000;
     key_val = 8'b0000_0000;
-	led_val = 1'b0;
+    row_stop = 1'b0;
     if (cols_sync != 4'b1111) begin // receives an input so one goes low
+        row_stop = 1'b1;
         key_col = ~cols_sync; // inverts bits so that col ON is in one hot encoding
-        key_val = {~rows, key_col};
-		led_val = 1'b1;
-		led_col = cols_sync[2:0];
+		key_val = {~key_row, key_col};
     end
+
 end
 
 endmodule
