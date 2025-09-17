75e6ad8d9fbfe15c8283104ff5dd728f7cf60117
juliacygong <jgong@g.hmc.edu>
2025-09-17 03:46:51 -0700

    debouncing keypadLUT.sv

--- lab03/fpga/src/keypadLUT.sv
+++ lab03/fpga/src/keypadLUT.sv
@@ -8,11 +8,16 @@ module keypadLUT(input logic [7:0] key_out,
                  input logic key_valid,
                  output logic [3:0] key);
 
+logic [7:0] key_real;
+
+
 // look up table for row and column inputs to keys
 always_comb begin
+	key = 4'b0000;
+	key_real = {key_out[5], key_out[4], key_out[7], key_out[6] , key_out[3:0]};
     if (key_valid) begin
         // outputs signal for 7-segment display
-        case (key_out)
+        case (key_real)
             8'b0001_0001: key = 4'b1010; // A
             8'b0001_0010: key = 4'b0000; // 0
             8'b0001_0100: key = 4'b1011; // B
@@ -29,7 +34,7 @@ always_comb begin
             8'b1000_0010: key = 4'b0010; // 2
             8'b1000_0100: key = 4'b0011; // 3
             8'b1000_1000: key = 4'b1100; // C
-            default: key = 4'b0000;
+            default: 	   key = 4'b0000;
         endcase
     end
 end
